9ec053a09853876eb760548f6c5a8f04c6309bf8
nikita sonawane <nrsonawane27@gmail.com>
2024-08-14 22:43:26 +0530

    new commit

--- src/Array/SubArray.java
+++ src/Array/SubArray.java
@@ -1,4 +1,37 @@
 package Array;
 
+import java.util.Scanner;
+
 public class SubArray {
+    public static void sub(int number[]){
+        for(int i=0;i<number.length;i++){
+            for(int j=i;j<number.length;j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print( +number[k]+"  ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n ;
+        System.out.println("Enter the array size :");
+        n = sc.nextInt();
+        int number[] = new int[n];
+
+        System.out.println("length of array:"+number.length);
+
+        for(int i=0;i<number.length;i++){
+            System.out.print("Enter the array elements =");
+            number[i] = sc.nextInt();
+        }
+
+        //subarray of array
+        System.out.println("\nsubarrays of array\n");
+        sub(number);
+
+
+    }
 }
