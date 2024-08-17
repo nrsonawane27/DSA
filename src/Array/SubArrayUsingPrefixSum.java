aa0a5d8bc1c33c472b5294ed7ad7c72b9891c26e
nikita sonawane <nrsonawane27@gmail.com>
2024-08-18 00:17:25 +0530

    new commit

--- src/Array/SubArrayUsingPrefixSum.java
+++ src/Array/SubArrayUsingPrefixSum.java
@@ -6,23 +6,24 @@ package Array;
 public class SubArrayUsingPrefixSum {
 
     public static void sub(int number[]){
-        int maxSum=Integer.MIN_VALUE;
-        int currSum=0;
-        int prefix[]=new int [number.length];
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        int prefix[] = new int [number.length];
 
-        prefix[0]=number[0];
+        prefix[0] = number[0];
         //calculate prefix array
-        for (int i=1;i<prefix.length;i++){
-            prefix[i]=prefix[i-1]+number[i];
+        for (int i=1; i<prefix.length; i++){
+            prefix[i] = prefix[i-1]+number[i];
         }
 
-        for(int i=0;i<number.length;i++){
+        //find max sum of subarray
+        for(int i=0; i<number.length; i++){
             for(int j=i;j<number.length;j++){
 
-                currSum = i== 0 ? prefix[j] : prefix[j]-prefix[i-1];
+                currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i-1];
 
-                if(maxSum<currSum){
-                    maxSum=currSum;
+                if(maxSum < currSum){
+                    maxSum = currSum;
                 }
             }
         }
