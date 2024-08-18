2ea9d85c93d06c063e73fb6758c9d70f280637d6
nikita sonawane <nrsonawane27@gmail.com>
2024-08-19 00:42:48 +0530

    new commit

---
+++ src/Array/RightMax.java
@@ -0,0 +1,26 @@
+/*
+   Implementation of carry forward method
+    using carry forward method change the element of array with the current max number
+    until the next element is greater than current element from side right side of array
+ */
+package Array;
+public class RightMax {
+    public static void main(String [] args) {
+        int []arr = {-3, 6, 2, 4, 5, 2, 8, -9, 3, 1};
+
+
+        int []rightmax = new int[10];
+        rightmax[9] = arr[9];
+        for(int i=arr.length-2; i>=0; i--) {
+            if(arr[i] > rightmax[i+1]){
+                rightmax[i] = arr[i];
+            } else {
+                rightmax[i] = rightmax[i+1];
+            }
+        }
+
+        for(int i = 0; i<rightmax.length; i++) {
+            System.out.print(rightmax[i]+ " ");
+        }
+    }
+}
