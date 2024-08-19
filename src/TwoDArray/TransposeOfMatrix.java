13f0763887d44f9756358e4302eb5fa5821b9deb
nikita sonawane <nrsonawane27@gmail.com>
2024-08-20 01:07:37 +0530

    new commit

--- src/TwoDArray/TransposeOfMatrix.java
+++ src/TwoDArray/TransposeOfMatrix.java
@@ -1,7 +1,18 @@
 package TwoDArray;
 
 public class TransposeOfMatrix {
+
+    public static void print(int [][]arr) {
+        for(int i = 0; i<=arr.length-1; i++) {
+            for(int j=0; j<=arr[0].length-1; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
+
+        //Transpose of square matrix
         int [][]arr = new int[4][5];
         arr[0] = new int[]{1,2,3,4};
         arr[1] = new int[]{5,6,7,8};
@@ -16,13 +27,27 @@ public class TransposeOfMatrix {
             }
         }
 
+        System.out.println("Square matrix Transpose");
+        print(arr);
 
 
-        for(int i = 0; i<=arr.length-1; i++) {
-            for(int j=0; j<=arr[0].length-1; j++) {
-                System.out.print(arr[i][j]+" ");
+        //transpose of rectangular matrix
+        int [][]arr2 = new int[4][5];
+        arr2[0] = new int[]{1,2,3,4,5};
+        arr2[1] = new int[]{6,7,8,9,10};
+        arr2[2] = new int[]{11,12,13,14,15};
+        arr2[3] = new int[]{16,17,18,19,20};
+
+        int N = arr2.length;   // row size
+        int M = arr2[0].length; //column size
+        int newarr[][] = new int[M][N];
+
+        for(int i = 0; i< N; i++) {
+            for(int j = 0; j < M; j++) {
+                newarr[j][i] = arr2[i][j];
             }
-            System.out.println();
         }
+
+        print(arr2);
     }
 }
