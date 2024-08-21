49841c159adeb22f166b9fa62b9f3482024f6453
nikita sonawane <nrsonawane27@gmail.com>
2024-08-21 18:26:00 +0530

    new commit

---
+++ src/Array/SlidingWindow.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class SlidingWindow {
+    public static void main(String[] args) {
+        int []arr = new int[]{-3, 4, -2, 5, 3, -2, 8, 2, 1, 4};
+
+        int k = 4;
+
+        int sum = 0;
+        for(int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        //sliding window concept
+        int maxSum = sum;
+        for( int i = 1; i <= arr.length-k; i++) {
+            sum = sum - arr[i-1] + arr[i+k-1];
+
+            if(sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+
+        System.out.println(maxSum);
+    }
+}
