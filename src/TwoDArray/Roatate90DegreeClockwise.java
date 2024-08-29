483fa1fef002d0ef48d54e2b1f4752e458a11566
nikita sonawane <nrsonawane27@gmail.com>
2024-08-29 21:37:16 +0530

    new commit

---
+++ src/TwoDArray/Roatate90DegreeClockwise.java
@@ -0,0 +1,35 @@
+package TwoDArray;
+
+public class Roatate90DegreeClockwise {
+    public static void main(String[] args) {
+        int [][]arr = new int[4][4];
+        arr[0] = new int[]{1,2,3,4};
+        arr[1] = new int[]{5,6,7,8};
+        arr[2] = new int[]{9,10,11,12};
+        arr[3] = new int[]{13,14,15,16};
+
+        for(int i = 0; i<=arr.length-1; i++) {
+            for(int j=i+1; j<=arr.length-1; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+            int m = arr[0].length-1;
+            for(int j = 0; j < arr[0].length/2; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[i][m];
+                arr[i][m] = temp;
+                m--;
+            }
+        }
+        for(int i = 0; i<=arr.length-1; i++) {
+            for(int j=0; j<=arr[0].length-1; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
