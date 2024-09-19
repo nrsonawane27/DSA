f6115694936b75723c6df651f2cfe84f5947df82
nikita sonawane <nrsonawane27@gmail.com>
2024-09-19 21:16:28 +0530

    Bubble Sort

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -7,25 +7,22 @@ public class BubbleSort {
         int N = arr.length;
 
         int count = 0;
+        boolean swapped = false;
         for(int i = 0; i < N-1; i++) {
             for(int j = 0; j < N-i-1; j++) {
                 if(arr[j] > arr[j+1]) {
                     int temp = arr[j+1];
                     arr[j+1] = arr[j];
                     arr[j] = temp;
-//                    swapped = true;
+                    swapped = true;
                 }
                 count++;
-//                if(swapped == false) {
-//                    break;
-//                }
+                if(swapped == false) {
+                    break;
+                }
             }
-
-//            if(swapped == false) {
-//                break;
-//            }
         }
-        System.out.println(count);
+        System.out.println("\nCount of iterations : " +count);
     }
 
     public static void print(int arr[]) {
@@ -34,8 +31,11 @@ public class BubbleSort {
             System.out.print(arr[i] +" ");
         }
     }
+
     public static void main(String[] args) {
-        int arr[] = {64, 34, 25, 12, 22, 11};
+//        int arr[] = {64, 34, 25, 12, 22, 11};
+        int arr[] = {2, 3, 4, 6, 8, 7};
+//        int arr[] = {2, 3, 4, 6, 5, 7, 9, 8};
 
         print(arr);
         bubbleSort(arr);
