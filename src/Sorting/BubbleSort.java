aa90b979f2d3d752207639b48d56f0f49312d651
nikita sonawane <nrsonawane27@gmail.com>
2024-09-19 20:56:57 +0530

    Bubble Sort

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -2,4 +2,44 @@
 package Sorting;
 
 public class BubbleSort {
+
+    public static void bubbleSort(int arr[]) {
+        int N = arr.length;
+
+        int count = 0;
+        for(int i = 0; i < N-1; i++) {
+            for(int j = 0; j < N-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+//                    swapped = true;
+                }
+                count++;
+//                if(swapped == false) {
+//                    break;
+//                }
+            }
+
+//            if(swapped == false) {
+//                break;
+//            }
+        }
+        System.out.println(count);
+    }
+
+    public static void print(int arr[]) {
+        int N = arr.length;
+        for(int i = 0; i < N; i++) {
+            System.out.print(arr[i] +" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {64, 34, 25, 12, 22, 11};
+
+        print(arr);
+        bubbleSort(arr);
+        System.out.println();
+        print(arr);
+    }
 }
