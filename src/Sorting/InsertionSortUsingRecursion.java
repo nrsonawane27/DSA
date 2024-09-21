4a06bb40c08509b13329a19e226ff6256aa8836d
nikita sonawane <nrsonawane27@gmail.com>
2024-09-21 22:51:06 +0530

    Insertion Sort using recursion

--- src/Sorting/InsertionSortUsingRecursion.java
+++ src/Sorting/InsertionSortUsingRecursion.java
@@ -6,14 +6,21 @@ public class InsertionSortUsingRecursion {
 
         if( i == arr.length)
             return;
+
         int element = arr[i];
         int j = i-1;
+        boolean swapped = false;
 
         while(j >= 0 && arr[j] > element) {
             arr[j+1] =arr[j];
             j--;
+            swapped = true;
         }
         arr[j+1] = element;
+
+        if(swapped == false) {
+            return;
+        }
 
         insertionSort(arr, i+1);
     }
@@ -26,8 +33,8 @@ public class InsertionSortUsingRecursion {
     }
 
     public static void main(String[] args) {
-        int arr[] = {64, 34, 25, 12, 22, 11};
-//        int arr[] = {2, 3, 4, 6, 8, 7};
+//        int arr[] = {64, 34, 25, 12, 22, 11};
+        int arr[] = {2, 3, 4, 6, 8, 7};
 //        int arr[] = {2, 3, 4, 6, 5, 7, 9, 8};
 
         print(arr);
