a9fdf929f85fcb5e09bf32ee29831e8e5ee06a0d
nikita sonawane <nrsonawane27@gmail.com>
2024-09-29 23:03:23 +0530

    Remove element from specific index

---
+++ src/Array/Basic/RemoveElementAtIndexFromArray.java
@@ -0,0 +1,28 @@
+package Array.Basic;
+
+public class RemoveElementAtIndexFromArray {
+    public static void main(String[] args) {
+//        int arr[] = new int[]{1,2,3,4,5};
+        int arr[] = new int[]{4,5,9,8,1};
+
+        int index = 3;
+        int N = arr.length;
+        int i ;
+
+        for(i = 0; i < N; i++) {
+            if(i == index) {
+                break;
+            }
+        }
+
+        for(int j = i; j < N-1; j++) {
+            arr[j] = arr[j+1];
+        }
+
+        for(i = 0; i < N-1; i++) {
+            System.out.print(arr[i] +" ");
+        }
+    }
+
+
+}
