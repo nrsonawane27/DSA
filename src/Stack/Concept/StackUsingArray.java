5d4aa7a51bc30a5cccfdd15a7f4fa6a3d4c7e6d3
nikita sonawane <nrsonawane27@gmail.com>
2024-10-04 23:12:40 +0530

    Stack Implementation using array

--- src/Stack/Concept/StackUsingArray.java
+++ src/Stack/Concept/StackUsingArray.java
@@ -1,4 +1,23 @@
 package Stack.Concept;
 
 public class StackUsingArray {
+
+    public class Stack {
+        int maxSize;
+        int stackArr[];
+        int top = -1;
+
+        Stack(int size) {
+            this.stackArr = new int[size];
+            this.maxSize = size;
+        }
+    }
+
+    int push(int element) {
+        if('' == )
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
