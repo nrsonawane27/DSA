51a17222c9157bece1a6f05f6e27faceafdd48a6
nikita sonawane <nrsonawane27@gmail.com>
2024-10-05 23:40:42 +0530

    Stack Implementation using linkedlist

---
+++ src/Stack/Concept/StackUsingLinkedList.java
@@ -0,0 +1,15 @@
+package Stack.Concept;
+
+public class StackUsingLinkedList {
+
+    public class Node {
+        int data;
+        Node next = null;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+
+}
