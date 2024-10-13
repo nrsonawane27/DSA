f3c9b259bad0e28a0417bdf5059bb8f6dbc133b9
nikita sonawane <nrsonawane27@gmail.com>
2024-10-13 23:18:53 +0530

    Closest Number

--- src/Array/Basic/ClosestNumber.java
+++ src/Array/Basic/ClosestNumber.java
@@ -25,7 +25,7 @@ public class ClosestNumber {
     }
     public static void main(String[] args) {
 //        int arr[] = {1, 3, 6, 7};
-        int arr[] = {1, 2, 3, 5, 9};
+        int[] arr = {1, 2, 3, 5, 9};
 
         System.out.println("Closest number of K from array : " +closestNum(arr, 7));
     }
