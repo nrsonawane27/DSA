52b0a873116cb686d92a2f4603bb19ac09103cdd
nikita sonawane <nrsonawane27@gmail.com>
2024-10-14 23:55:54 +0530

    Valid parenthesis

---
+++ src/Stack/Assignment/CheckParenthesis.java
@@ -0,0 +1,49 @@
+package Stack.Assignment;
+
+import java.util.Stack;
+
+public class CheckParenthesis {
+
+    static boolean validParenthesis (String str) {
+        Stack<Character> s = new Stack<>();
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch == '{' || ch == '(' || ch == '[') {
+                s.push(ch);
+            } else {
+                if(! s.empty()) {
+                    char x = s.peek();
+                    if((x == '{' && ch == '}') || (x == '(' && ch == ')') || (x == '[' && ch == ']')) {
+                        s.pop();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if(s.empty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+//        String str = "{([(){}])}";
+        String str = "{([()}])}";
+//        String str = "{([(){}])}{[(";
+//        String str = "{([(){}])}])";
+//        String str = "()";
+
+
+        if(validParenthesis(str)) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+
+    }
+}
