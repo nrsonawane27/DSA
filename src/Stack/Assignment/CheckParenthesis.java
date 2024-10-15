abf052e62b3ce095a53d48c030854dfb5e067d3f
nikita sonawane <nrsonawane27@gmail.com>
2024-10-15 23:41:28 +0530

    Valid parenthesis

--- src/Stack/Assignment/CheckParenthesis.java
+++ src/Stack/Assignment/CheckParenthesis.java
@@ -29,9 +29,9 @@ public class CheckParenthesis {
 
     public static void main(String[] args) {
 //        String str = "{([(){}])}";
-//        String str = "{([()}])}";
+        String str = "{([()}])}";
 //        String str = "{([(){}])}{[(";
-        String str = "{([(){}])}])";
+//        String str = "{([(){}])}])";
 //        String str = "()";
 
         if(validParenthesis(str)) {
