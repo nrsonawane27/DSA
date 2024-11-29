899e1e2ad48986a22857bba2e26b446f20f0165c
nikita sonawane <nrsonawane27@gmail.com>
2024-11-29 23:45:31 +0530

    Binary tree

---
+++ src/Tree/BinaryTree/BinaryTreeImplementation.java
@@ -0,0 +1,26 @@
+package Tree.BinaryTree;
+
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node right;
+    Node left;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+    Scanner sc = new Scanner(System.in);
+
+    Node ConstructBt() {
+        System.out.println();
+    }
+}
+
+
+public class BinaryTreeImplementation {
+}
